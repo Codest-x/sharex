ecc6bae2c279cf64d27665ea0be929ef8af950c5
Esteban Estrada <davinsontc@outlook.com>
2022-03-07 18:13:54 -0500

    fix: listen changes in user to verify authentication

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 /* eslint-disable eqeqeq */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectUser } from './features/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectUser, login, logout } from './features/userSlice'
 import Header from './components/Header/header-component'
 import { Home } from './pages'
 import Login from './pages/Login/login'
+import { onAuthStateChanged, getAuth } from 'firebase/auth'
 
 function App() {
+  const dispatch = useDispatch()
+  const auth = getAuth()
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          login({
+            email: user.email,
+            uid: user.uid,
+            displayName: user.displayName,
+            photoUrl: user.photoURL
+          })
+        )
+        // ...
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [])
   const location = useLocation()
   const user = useSelector(selectUser)
   return (
     <div className="app">
       {location.pathname != '/login' ? <Header /> : null}
       <Routes>
-        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-        <Route path="/" element={!user ? <Navigate to="/login" /> : <Home />} />
+        <Route path="/login" element={user.user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/" element={!user.user ? <Navigate to="/login" /> : <Home />} />
       </Routes>
     </div>
   )
