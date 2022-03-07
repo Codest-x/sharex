ba27bc5d2aca4a28f7cda352d241b7afe68032a2
Esteban Estrada <davinsontc@outlook.com>
2022-03-07 15:06:10 -0500

    adding login route and user route auth

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from 'react'
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { selectUser } from './features/userSlice'
-import Feed from './components/Feed/feed-component'
 import Header from './components/Header/header-component'
-import Sidebar from './components/Sidebar/sidebar-component'
 import { Home } from './pages'
 import Login from './pages/Login/login'
 
@@ -15,13 +13,9 @@ function App() {
   return (
     <div className="app">
       {location.pathname != '/login' ? <Header /> : null}
-      <div className="app__body">
-        {location.pathname != '/login' ? <Sidebar /> : null}
-        {location.pathname != '/login' ? <Feed /> : null}
-      </div>
       <Routes>
-        <Route path="/" element={!user ? <Navigate to="/login" /> : <Home />} />
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/" element={!user ? <Navigate to="/login" /> : <Home />} />
       </Routes>
     </div>
   )
