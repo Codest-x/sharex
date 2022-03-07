bf3e1b2c79c0ddc32c985f132001701c4c94b1ff
Esteban Estrada <davinsontc@outlook.com>
2022-03-07 15:06:56 -0500

    fix: move sidebar and feed to home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import Feed from '../../components/Feed/feed-component'
+import Sidebar from '../../components/Sidebar/sidebar-component'
 
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="app__body">
+      {location.pathname === '/login' ? null : <Sidebar />}
+      {location.pathname === '/login' ? null : <Feed />}
+    </div>
+  )
 }
