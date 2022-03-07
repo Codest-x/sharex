5beef0fa166597a2a59dd5999af5cfa83145470f
Esteban Estrada <davinsontc@outlook.com>
2022-03-07 15:13:19 -0500

    fix: moving form to separate component

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,19 +1,9 @@
-/* eslint-disable multiline-ternary */
-import React, { useState } from 'react'
+import React from 'react'
 import './login.scss'
 import LoginBg from '../../assets/login-bg.mp4'
-import Button from '../../components/Button/button-component'
+import Form from '../../components/Form/form-component'
 
 export default function Login() {
-  const [loginState, setLoginState] = useState({
-    current: 'register',
-    title: 'Iniciar Sesion'
-  })
-  const changeForm = (e) => {
-    e.target.innerHTML === 'Iniciar Sesion'
-      ? setLoginState({ current: 'login', title: 'Registarme' })
-      : setLoginState({ current: 'register', title: 'Iniciar Sesion' })
-  }
   return (
     <div className="login__container">
       <div className="login__left">
@@ -32,54 +22,7 @@ export default function Login() {
         </div>
       </div>
       <div className="login__right">
-        <div className="form__container">
-          <form
-            style={
-              loginState.current === 'login'
-                ? { display: 'none' }
-                : { display: '' }
-            }
-          >
-            <div className="text__box" style={{ gap: '0px' }}>
-              <h1 className="anim-typewriter" style={{ padding: '0 5px' }}>
-                Regis
-              </h1>
-              <h1>trarme</h1>
-            </div>
-            <input type="text" placeholder="Nombre Completo" />
-            <input type="text" placeholder="Correo Electronico" />
-            <input type="text" placeholder="Contraseña" />
-            <input type="text" placeholder="Confirmar Contraseña" />
-            <Button type="primary" size="medium" width="150px">
-              Registrarme
-            </Button>
-          </form>
-          <form
-            style={
-              loginState.current === 'register'
-                ? { display: 'none' }
-                : { display: '' }
-            }
-          >
-            <div className="text__box">
-              <h1 className="anim-typewriter">Iniciar</h1>
-              <h1>Sesion</h1>
-            </div>
-            <input type="text" placeholder="Correo Electronico" />
-            <input type="text" placeholder="Contraseña" />
-            <Button type="primary" size="medium" width="150px">
-              Iniciar Sesion
-            </Button>
-          </form>
-          <div className="form__changeform">
-            <span>
-              {loginState.current === 'register'
-                ? '¿ Ya tienes una cuenta ?'
-                : '¿ No tienes una cuenta ?'}
-            </span>
-            <button onClick={changeForm}>{loginState.title}</button>
-          </div>
-        </div>
+        <Form />
       </div>
     </div>
   )
