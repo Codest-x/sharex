8776756526928bcd678600e82055acb4fe364b3f
Esteban Estrada <davinsontc@outlook.com>
2022-03-08 07:41:49 -0500

    feat: adding widgest section

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Feed from '../../components/Feed/feed-component'
 import Sidebar from '../../components/Sidebar/sidebar-component'
+import Widgets from '../../components/Widgets/widgets-component'
 
 export default function Home() {
   return (
     <div className="app__body">
       {location.pathname === '/login' ? null : <Sidebar />}
       {location.pathname === '/login' ? null : <Feed />}
+      {location.pathname === '/login' ? null : <Widgets />}
     </div>
   )
 }
