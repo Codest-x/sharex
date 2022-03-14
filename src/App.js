3afd6cab9054b4aa51499fadb7b20c1af435425e
Esteban Estrada <davinsontc@outlook.com>
2022-03-14 00:03:27 -0500

    fix: message alert for no verificated emails

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ import { selectUser, login, logout } from './features/userSlice'
 import Header from './components/Header/header-component'
 import { Home } from './pages'
 import Login from './pages/Login/login'
-import {
-  onAuthStateChanged,
-  getAuth
-} from 'firebase/auth'
+import { onAuthStateChanged, getAuth } from 'firebase/auth'
 import Message from './components/Messages/VerificationMessage/verification-message'
 
 function App() {
@@ -43,9 +40,9 @@ function App() {
   return (
     <div className="app">
       {location.pathname != '/login' ? (
-        user?.isVerified ? null : (
+        user?.isVerified === false ? (
           <Message email={user?.email} />
-        )
+        ) : null
       ) : null}
       {location.pathname != '/login' ? <Header /> : null}
       <Routes>
