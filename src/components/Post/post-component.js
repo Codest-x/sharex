975800052365d638b2e0bb0e039a4cc2940428cc
Esteban Estrada <davinsontc@outlook.com>
2022-03-14 00:05:41 -0500

    feat: adding commentInput component and design

--- src/components/Post/post-component.js
+++ src/components/Post/post-component.js
@@ -1,6 +1,6 @@
-/* eslint-disable space-before-function-paren */
+/* eslint-disable multiline-ternary */
 /* eslint-disable react/display-name */
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useState } from 'react'
 import './post.scss'
 import PropTypes from 'prop-types'
 import Avatar from '@mui/material/Avatar'
@@ -12,74 +12,101 @@ import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined'
 import SendOutlinedIcon from '@mui/icons-material/SendOutlined'
 import { useSelector } from 'react-redux'
 import { selectUser } from '../../features/userSlice'
-import { doc, updateDoc } from 'firebase/firestore'
-import { db } from '../../firebase/firebase'
+import { addLiketoPost, addComment } from './actions'
+import Fade from 'react-reveal/Fade'
 
 const Post = forwardRef(
-  ({ postId, name, description, message, photoUrl, reactions }, ref) => {
-    const {
-      user: { uid }
-    } = useSelector(selectUser)
-    const { usersLikes } = reactions
+  (
+    {
+      postId,
+      name,
+      description,
+      message,
+      photoUrl,
+      usersLikes,
+      usersShares,
+      comments
+    },
+    ref
+  ) => {
+    const { user } = useSelector(selectUser)
+    const [showCommentInput, setShowCommentInput] = useState(false)
+    const [commentInput, setCommentInput] = useState('')
 
-    const removeItemOnce = (arr, value) => {
-      const index = arr.indexOf(value)
-      if (index > -1) {
-        arr.splice(index, 1)
-      }
-      return arr
-    }
-
-    const addLiketoPost = async () => {
-      if (usersLikes.includes(uid)) {
-        await updateDoc(doc(db, 'posts', postId), {
-          reactions: {
-            ...reactions,
-            usersLikes: removeItemOnce(usersLikes, uid)
-          }
-        })
-      } else {
-        await updateDoc(doc(db, 'posts', postId), {
-          reactions: {
-            ...reactions,
-            usersLikes: usersLikes.concat(uid)
-          }
-        })
-      }
-    }
     return (
-      <div ref={ref} className="post">
-        <div className="post__header">
-          <Avatar src={photoUrl}>{name[0]}</Avatar>
-          <div className="post__info">
-            <h2>{name}</h2>
-            <p>{description}</p>
+      <>
+        <div ref={ref} className="post">
+          <div className="post__header">
+            <Avatar src={photoUrl}>{name[0]}</Avatar>
+            <div className="post__info">
+              <h2>{name}</h2>
+              <p>{description}</p>
+            </div>
           </div>
-        </div>
-        <div className="post__body">
-          <p>{message}</p>
-        </div>
-        <div className="post__reactions">
-          <span>{`Likes: ${reactions.usersLikes.length}`}</span>
-          <div>
-            <span>{`Comentarios: ${reactions.comments}`}</span>
-            <span>{`Compartidas: ${reactions.shares}`}</span>
+          <div className="post__body">
+            <p>{message}</p>
           </div>
-        </div>
+          <div className="post__reactions">
+            <span>{`${usersLikes.length} Likes`}</span>
+            <div>
+              <span className="postReactions__comments">{`${comments.length} Comentarios`}</span>
+              <span>{`${usersShares} Compartidas`}</span>
+            </div>
+          </div>
 
-        <div className="post__buttons">
-          <InputOption
-            Icon={usersLikes.includes(uid) ? ThumbUpIcon : ThumbUpAltOutlinedIcon}
-            title="Like"
-            color={usersLikes.includes(uid) ? 'blue' : ''}
-            onClick={addLiketoPost}
-            active={usersLikes.includes(uid) ? 'blue' : ''}
-          />
-          <InputOption Icon={InsertCommentOutlinedIcon} title="Comentar" />
-          <InputOption Icon={ShareOutlinedIcon} title="Compartir" />
-          {/* <InputOption Icon={SendOutlinedIcon} title="Enviar" /> */}
+          <div className="post__buttons">
+            <InputOption
+              Icon={
+                usersLikes.includes(user.uid)
+                  ? ThumbUpIcon
+                  : ThumbUpAltOutlinedIcon
+              }
+              title="Like"
+              color={usersLikes.includes(user.uid) ? 'red' : ''}
+              onClick={() => {
+                addLiketoPost(user, usersLikes, postId)
+              }}
+              active={usersLikes.includes(user.uid) ? 'red' : ''}
+            />
+            <InputOption
+              Icon={InsertCommentOutlinedIcon}
+              onClick={() => {
+                setShowCommentInput(!showCommentInput)
+              }}
+              title="Comentar"
+            />
+            <InputOption Icon={ShareOutlinedIcon} title="Compartir" />
+            {/* <InputOption Icon={SendOutlinedIcon} title="Enviar" /> */}
+          </div>
         </div>
-      </div>
+        <Fade top when={showCommentInput}>
+          <div
+            className="postInput__comment"
+            style={showCommentInput ? { display: 'flex' } : { display: 'none' }}
+          >
+            <Avatar src={user.photoUrl}>{user?.displayName[0]}</Avatar>
+            <div>
+              <form>
+                <input
+                  placeholder="Escribir comentario"
+                  value={commentInput}
+                  onChange={({ target }) => setCommentInput(target.value)}
+                />
+                <button
+                  onClick={(e) => {
+                    addComment(e, user, commentInput, comments, postId)
+                    setCommentInput('')
+                  }}
+                  type="submit"
+                  className=""
+                >
+                  Send Comment
+                </button>
+              </form>
+            </div>
+          </div>
+        </Fade>
+      </>
     )
   }
 )
@@ -92,5 +119,7 @@ Post.propTypes = {
   description: PropTypes.string,
   message: PropTypes.string,
   photoUrl: PropTypes.string,
-  reactions: PropTypes.object
+  usersLikes: PropTypes.array,
+  usersShares: PropTypes.number,
+  comments: PropTypes.array
 }
