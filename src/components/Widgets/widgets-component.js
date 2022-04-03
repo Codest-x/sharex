772b006b59f4d97b071a150a9ffafe5e038c6ba4
Codest-x <davinsontc@outlook.com>
2022-04-03 00:15:39 -0500

    feat: adding url to view news

--- src/components/Widgets/widgets-component.js
+++ src/components/Widgets/widgets-component.js
@@ -2,26 +2,63 @@ import React from 'react'
 import './widget.scss'
 import InfoIcon from '@mui/icons-material/Info'
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'
+import PropTypes from 'prop-types'
 
 export default function Widgets() {
-  const newsArticle = (heading, subtitle) => (
-    <div className="widgets__article">
-      <div className="widgets__articleLeft">
-        <FiberManualRecordIcon />
+  const NewsArticle = ({ heading, subtitle, url }) => {
+    NewsArticle.propTypes = {
+      heading: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      url: PropTypes.string
+    }
+    return !url ? (
+      <div className="widgets__article">
+        <div className="widgets__articleLeft">
+          <FiberManualRecordIcon />
+        </div>
+        <div className="widgets__articleRight">
+          <h4>{heading}</h4>
+          <p>{subtitle}</p>
+        </div>
       </div>
-      <div className="widgets__articleRight">
-        <h4>{heading}</h4>
-        <p>{subtitle}</p>
-      </div>
-    </div>
-  )
+    ) : (
+      <a
+        className="widgets__article"
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="widgets__articleLeft">
+          <FiberManualRecordIcon />
+        </div>
+        <div className="widgets__articleRight">
+          <h4>{heading}</h4>
+          <p>{subtitle}</p>
+        </div>
+      </a>
+    )
+  }
   return (
     <div className="widgets">
       <div className="widgets__header">
-        <h2>Bookex News</h2>
+        <h2>ShareX News</h2>
         <InfoIcon />
       </div>
-      {newsArticle('Making a social media', 'Programming')}
+      <NewsArticle
+        heading="Watch My Portfolio"
+        subtitle="Works, Projects, and Skills"
+        url="https://codest.vercel.app"
+      />
+      <NewsArticle
+        heading="Follow My Twitter"
+        subtitle="@Codest_1x"
+        url="https://twitter.com/Codest_1x"
+      />
+      <NewsArticle
+        heading="Follow My Instagram"
+        subtitle="@Codest.x"
+        url="https://www.instagram.com/codest.x/"
+      />
     </div>
   )
 }
